d5d0c4338969c0964236f496d301ae8829f4c0cd
Russell Gordon <rgordon@lcs.on.ca>
2019-01-10 09:34:17 -0500

    added basic checks

---
+++ dde/check50/__init__.py
@@ -0,0 +1,18 @@
+from check50 import *
+
+class DDE(Checks):
+
+	@check()
+	def exists(self):
+		"""dde.c exists"""
+		self.require("dde.c")
+
+	@check("exists")
+	def compiles(self):
+		"""dde.c compiles"""
+		self.spawn("clang -o dde dde.c -lcs50 -lm").exit(0)
+
+	@check("compiles")
+	def test_base_case(self):
+		"""input from example provided in problem description provides correct output"""
+		self.spawn("./greedy").stdin("2\n", "the cat chased the rat while\n", "the dog chased the cat into the rat house\n").stdout("\n", "the cat chased 1 rat while\n", "1 dog 3 1 2 into 1 4 house\n").exit(0)
