26afa28fdd04a14f0ec7ef76cb475d735d214d72
Russell Gordon <rgordon@lcs.on.ca>
2019-01-10 10:01:13 -0500

    trying to fix check syntax

--- dde/check50/__init__.py
+++ dde/check50/__init__.py
@@ -15,4 +15,4 @@ class DDE(Checks):
 	@check("compiles")
 	def test_base_case(self):
 		"""input from example provided in problem description provides correct output"""
-		self.spawn("./dde").stdin("2\nthe cat chased the rat while\nthe dog chased the cat into the rat house\n").stdout("\nthe cat chased 1 rat while\n1 dog 3 1 2 into 1 4 house\n", "\nthe cat chased 1 rat while\n1 dog 3 1 2 into 1 4 house\n").exit(0)
+		self.spawn("./dde").stdin("2").stdin("the cat chased the rat while").stdin("the dog chased the cat into the rat house").stdout("").stdout("the cat chased 1 rat while").stdout("1 dog 3 1 2 into 1 4 house").exit(0)
