17056eed5bb8a4baa38c76e5d10ef961ac50f853
lcs-rgordon <32135742+lcs-rgordon@users.noreply.github.com>
2019-01-12 15:46:42 -0500

    Add zero lines of input check

--- dde/check50/__init__.py
+++ dde/check50/__init__.py
@@ -16,3 +16,9 @@ class DDE(Checks):
 	def test_base_case(self):
 		"""input from example provided in problem description provides correct output"""
 		self.spawn("./dde").stdin("2").stdin("the cat chased the rat while").stdin("the dog chased the cat into the rat house").stdout("").stdout("the cat chased 1 rat while").stdout("1 dog 3 1 2 into 1 4 house").exit(0)
+		
+	@check("compiles")
+	def test_no_input(self):
+		"""handles 0 lines of input correctly"""
+		self.spawn("./dde").stdin("0").stdout("").exit(0)
+		
