9da19a38da953f93f9db49a351fd61533d06b0f4
Russell Gordon <rgordon@lcs.on.ca>
2019-01-26 08:29:35 -0500

    Fixed typo in checks for S character

--- frequency/check50/__init__.py
+++ frequency/check50/__init__.py
@@ -15,4 +15,4 @@ class PartnerUp(Checks):
 	@check("compiles")
 	def test_base_case(self):
 		"""does the program provide the expected output from the example given?"""
-		self.spawn("./frequency").stdin("2").stdin("THE QUICK BROWN FOX").stdin("JUMPED OVER THE LAZY DOG").stdout("A *").stdout("B *").stdout("C *").stdout("D **").stdout("E ****").stdout("F *").stdout("G *").stdout("H **").stdout("I *").stdout("J *").stdout("K *").stdout("L *").stdout("M *").stdout("N *").stdout("O ****").stdout("P *").stdout("Q *").stdout("R **").stdout("T **").stdout("U **").stdout("V *").stdout("W *").stdout("X *").stdout("Y *").stdout("Z *").exit(0)
+		self.spawn("./frequency").stdin("2").stdin("THE QUICK BROWN FOX").stdin("JUMPED OVER THE LAZY DOG").stdout("A *").stdout("B *").stdout("C *").stdout("D **").stdout("E ****").stdout("F *").stdout("G *").stdout("H **").stdout("I *").stdout("J *").stdout("K *").stdout("L *").stdout("M *").stdout("N *").stdout("O ****").stdout("P *").stdout("Q *").stdout("R **").stdout("S").stdout("T **").stdout("U **").stdout("V *").stdout("W *").stdout("X *").stdout("Y *").stdout("Z *").exit(0)
